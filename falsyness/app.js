340d8f12ca4eec1a6c829b92d4c85fc9d6d85905
alex Mendoza <tiburon9386@gmail.com>
2021-09-09 08:40:48 -0500

    valores falsos

---
+++ falsyness/app.js
@@ -0,0 +1,13 @@
+// const usuarioInput = prompt("Ingrese un dato");
+
+// if (usuarioInput) {
+//     console.log("Verdadero")
+// } else {
+//     console.log("Falso")
+// }
+// false, 0, "", null, undefined, NaN son valores directamente falsos
+if (NaN) {
+    console.log("Verdadero")
+} else {
+    console.log("Falso")
+}
