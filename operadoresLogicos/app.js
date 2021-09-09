0c5547afe7d1c03c3274f57da989370b1a6367f6
alex Mendoza <tiburon9386@gmail.com>
2021-09-09 10:18:37 -0500

    edad invalida

--- operadoresLogicos/app.js
+++ operadoresLogicos/app.js
@@ -7,5 +7,5 @@ if (age >= 0 && age <= 5 || age >= 65 && age < 120) {
 } else if (age > 10 && age < 65) {
     console.log("Pagas 200 pesos")
 } else {
-    console.log("Numero invalido")
+    console.log("Edad invalida")
 }
