1558e7b7c5a5ffba3bee1ffbce8e7e314391d5d7
alex Mendoza <tiburon9386@gmail.com>
2021-09-14 10:30:28 -0500

    hicimos aplicacion de chistes con API

---
+++ API-dad-joke/app.js
@@ -0,0 +1,22 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLi = document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi)
+}
+const getDadJoke = async () => {
+    try{
+ const config = {headers: {Accept: 'application/json'}};
+ const res = await axios.get('https://icanhazdadjoke.com/', config);
+ return res.data.joke;
+}catch(e){
+    return "Error, no hay chistes disponibles"
+}
+       
+}
+
+// getDadJoke()
+button.addEventListener('click', addNewJoke)
