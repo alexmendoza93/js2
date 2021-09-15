fcc2d74416f29407210250ae806f6d7e3efbe309
alex Mendoza <tiburon9386@gmail.com>
2021-09-15 12:41:26 -0500

    aprendi a exportar modulos de otros archivos

---
+++ node/exportarModulos/app.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+console.log(math.add(49,54))
+console.log(math.PI)
+console.log(math.multiply(38,82))
+console.log(math)
+
+// se corre desde terminal con: node app.js
---
+++ node/exportarModulos/math.js
@@ -0,0 +1,23 @@
+// const add = (x,y) => x+y;
+// const PI = 3.141592;
+// const multiply = (x,y) => x*y;
+
+// module.exports.add = add
+// module.exports.PI = PI
+// module.exports.multiply = multiply
+
+// --------------------------------
+// otras formas de exportar modulos es:
+
+// const math ={
+//     add: add,
+//     PI: PI,
+//     multiply: multiply
+// }
+
+// module.exports = math;
+
+// -----------------------------------------
+module.exports.add = (x,y) => x+y;
+module.exports.PI = 3.141592;
+module.exports.multiply = (x,y) => x*y;
