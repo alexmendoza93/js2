206a26f869af4206a7d4c59a3bd06ce9da48b902
alex Mendoza <tiburon9386@gmail.com>
2021-09-15 16:45:31 -0500

    todo estaba jalando super bien pero de pronto fallo

---
+++ express/routing/index.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express();
+
+// el metodo use se corre cada vez que hay un nuevo request
+app.use(()=>{
+    console.log('tenemos un nuevo request')
+})
+
+// el metodo get es como mandamos respuestas especificas dependiendo la direccion url
+app.get('/',(req,res)=>{
+    res.send('Este es el Home Page')
+})
+
+
+app.get('/cat',(req,res)=>{
+    res.send('MEOWWW')
+})
+app.get('/dog',(req,res)=>{
+    res.send('WOOFF')
+})
+app.get('*',(req,res)=>{
+    res.send('Lo siento no se encontro la pagina')
+})
+
+// ahora iniciamos el servidor con: node index.js cada vez que cambiemos algo (para salir de node es (ctrl + c dos veces)
+
+app.listen((8080), ()=>{
+    console.log('escuchando desde el puerto 8080')
+})
