167ec6a6312e6ccb6bfe8b3cb87112fc85447ce6
alex Mendoza <tiburon9386@gmail.com>
2021-09-21 12:38:12 -0500

    ahora podemos ver la info de un solo comentario a detalle y agregamos id a nuevo comentario

--- RESTful/app.js
+++ RESTful/app.js
@@ -12,22 +12,27 @@ app.set('view engine', 'ejs');
 // ahora simularemos una base de datos
 const comentarios = [
     {
+        id:1,
         username:'aldo',
         comentario: 'that is hilarious'
     },
     {
+        id:2,
         username:'alex',
         comentario: 'that is sooo funny'
     },
     {
+        id:3,
         username:'armando',
         comentario: 'hahahahaha that´s great'
     },
     {
+        id:4,
         username:'annita',
         comentario: 'I don´t really like it'
     },
     {
+        id:5,
         username:'alfaro',
         comentario: 'so stupid'
     }
@@ -48,9 +53,26 @@ app.post('/comments', (req, res) =>{
     console.log(req.body);
     // esto es lo que hice para ver si funcionaba
     const {username, comentario }= req.body;
-    comentarios.push({ username, comentario });
+    let id = comentarios.length+1;
+    // esta forma nos sirve bien por el momento, en el original pusieron de id con un npm uuid
+    comentarios.push({ username, comentario, id });
     res.redirect('/comments')
     
+})
+
+// ahora vamos a definir nuestra ruta para buscar comentarios especificos
+app.get('/comments/:id', (req, res) =>{
+    const {id} = req.params;
+    console.log(req.params);
+    const comentario =comentarios.find(c => c.id === parseInt(id));
+    // ponemos parse int por que lo que da req.params es un string
+    // y esto se supone que deberia regresar un comentario
+    res.render('comentarios/show', {comentario});
+})
+
+// con patch podemos actualizar informacion
+app.patch('/comments/:id', (req, res) =>{
+    res.send('se actualizo el comentario')
 })
 
 
